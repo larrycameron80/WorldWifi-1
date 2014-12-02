2e1702ab49798f09f44143da1badd3679348ce05
Eduard <chory.eduard@thinkmobiles.com>
2014-12-02 17:18:31 +0200

    start

---
+++ src/com/example/WorldWifi/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.WorldWifi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Eduard
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
